da16e885cb15e27d6f7eb4ece71602016e3e1a95
zehuaz4 <46539586+blackpatton17@users.noreply.github.com>
2020-08-03 21:28:31 -0700

    finish but a has bug

--- problem-b/js/index.js
+++ problem-b/js/index.js
@@ -2,16 +2,22 @@
 
 /* Define a function `addFour()` that takes a single argument 
    and returns a value 4 greater than the input.*/
-
+const addFour = (input) => {
+  return input + 4;
+};
    
 /* Create and log a variable `twelve` that is the result of passing 8 to your
    addFour() function. */
-
+let twelve = addFour(8);
+console.log(twelve);
    
 /* Create and log a variable `twelveString` that is the result of passing "8" 
    (a string) to your addFour() function. Consider what this tells you about how
   the function should be explained (e.g., in a comment). */
-
+  // If the input is string, the function will consider to append 4 after the input
+  // string instead of parsing the input to a integer.
+let twelveString = addFour("8");
+console.log(twelveString);
   
 
 /* Define a function `compoundInterest()` that takes three parameters: 
@@ -25,7 +31,9 @@
    You can call the method and log the result to check your work. Compare to
      http://www.mathwarehouse.com/calculators/continuous-compound-interest-calculator.php
 */
-
+const compoundInterest = (balance, interest, yearNum) => {
+  return balance * Math.pow(Math.E, interest * yearNum);
+};
 
 
 /* Define a function `fizzBuzz()` that takes in a single number as an argument.
@@ -35,7 +43,16 @@
    number. For numbers which are multiples of both three and five, the array 
    should contain "FizzBuzz" instead of the number.
    The returned array should be empty for arguments less than 1. */
-
+const fizzBuzz = (int) => {
+  let output = new Array(int);
+  for (let i = 0; i < output.length; i++) {
+    output[i] =
+      ((i + 1) % 3 === 0 && (i + 1) % 5 === 0) ? "FizzBuzz" :
+        (((i + 1) % 3 === 0) ? "Fizz" :
+          ((i + 1) % 5 === 0) ? "Buzz" : i + 1);
+  }
+  return output;
+};
    
 
 /* Define a function `getLetterFrequencies()` that takes in a single string as 
@@ -47,7 +64,13 @@
    each letter, increase the value associated with that key by one. Watch out 
    for if the letter is not in the Object yet!
    You can test this method with a word like "Mississippi". */
-
+const getLetterFrequencies = (input) => {
+  let output = {};
+  input.split("").map(x => {
+    output[x] = !output[x] ? 1 : output[x] + 1;
+  });
+  return output;
+};
    
 
 /* Create a variable `deck` that represents a deck of modern playing cards
@@ -61,7 +84,14 @@
     the `deck` array! 
     
     You can log out the `deck` to check your work! */
+const SUITS = ["hearts", "diamonds", "clubs", "spades"];
 
+let deck = new Array(52);
+for (let i = 0; i < deck.length; i ++) {
+  let rank = i % (deck.length / SUITS.length) + 2;
+  let suit = SUITS[Math.floor(i / (deck.length / SUITS.length))];
+  deck[i] = {rank: rank, suit: suit};
+}
     
 
 //You can test the below functions by creating e.g., a `pokerHand` array that 
@@ -71,20 +101,31 @@
    objects (e.g., a Poker hand) and returns whether or not the Queen of Hearts
    is in that array.
    Hint: use a loop to check each card. */
-
-   
+const containsQueenOfHearts = (cards) => {
+  return cards.some((x) => {
+    return x.suit === SUITS[0] && x.rank === 12;
+  });
+};
 
 /* Define a function `getHighCard()` that takes in an array of "card" objects
   and returns the card object with the highest value. The "high card" is the one
   with the highest rank. Cards of different suits but the same rank are 
   considered to have the same value, and either is a valid result */
-
+const getHighCard = (cards) => {
+  cards.sort((a, b) => {
+    return (a.rank > b.rank) ? 1 : ((a.rank < b.rank) ? -1 : 0);
+  });
+  return cards[cards.length - 1];
+};
   
 
 /* Define a function `isFlush()` that takes in an array of "card" objects and
    returns whether or not the cards all have the same _suit_. */
-
-   
+const isFlush = (cards) => {
+  return !cards.some(x => {
+    return x.suit !== cards[0].suit;
+  });
+};
 
 /* Extra challenge: define a function `hasPair()` that takes in an array of 
    "card" objects and returns whether or not there is at least one _pair_ (two 
--- problem-c/js/index.js
+++ problem-c/js/index.js
@@ -3,31 +3,31 @@
 /* Below is an array of objects representing Husky Football games from the 2016
    season. Each object has the same properties, so it represents a data table. */
 let huskyGames2016 = [
-  { "date":"9/3/16", "home":"UW", "opponent":"Rutgers", "home_score":48, 
+  { "date":"9/3/16", "home":"UW", "opponent":"Rutgers", "home_score":48,
     "opponent_score":13, "passing_yards":289, "rushing_yards":91, "fumbles":0 },
-  { "date":"9/10/16", "home":"UW", "opponent":"Idaho", "home_score":59, 
+  { "date":"9/10/16", "home":"UW", "opponent":"Idaho", "home_score":59,
     "opponent_score":14, "passing_yards":356, "rushing_yards":126, "fumbles":1 },
   { "date":"9/17/16", "home":"UW", "opponent":"Portland State", "home_score":41, 
     "opponent_score":3, "passing_yards":194, "rushing_yards":213, "fumbles":1 },
-  { "date":"9/24/16", "home":"Arizona", "opponent":"UW", "home_score":28, 
+  { "date":"9/24/16", "home":"Arizona", "opponent":"UW", "home_score":28,
     "opponent_score":35, "passing_yards":160, "rushing_yards":352, "fumbles":0 },
-  { "date":"9/30/16", "home":"UW", "opponent":"Stanford", "home_score":44, 
+  { "date":"9/30/16", "home":"UW", "opponent":"Stanford", "home_score":44,
     "opponent_score":6, "passing_yards":210, "rushing_yards":214, "fumbles":0 },
-  { "date":"10/8/16", "home":"Oregon", "opponent":"UW", "home_score":21, 
+  { "date":"10/8/16", "home":"Oregon", "opponent":"UW", "home_score":21,
     "opponent_score":70, "passing_yards":304, "rushing_yards":378, "fumbles":0 },
-  { "date":"10/22/16", "home":"UW", "opponent":"Oregon State", "home_score":41, 
+  { "date":"10/22/16", "home":"UW", "opponent":"Oregon State", "home_score":41,
     "opponent_score":17, "passing_yards":300, "rushing_yards":219, "fumbles":2 },
-  { "date":"10/29/16", "home":"Utah", "opponent":"UW", "home_score":24, 
+  { "date":"10/29/16", "home":"Utah", "opponent":"UW", "home_score":24,
     "opponent_score":31, "passing_yards":186, "rushing_yards":199, "fumbles":1 },
-  { "date":"11/5/16", "home":"Cal", "opponent":"UW", "home_score":27, "opponent_score":66, 
+  { "date":"11/5/16", "home":"Cal", "opponent":"UW", "home_score":27, "opponent_score":66,
     "passing_yards":417, "rushing_yards":287, "fumbles":2 },
-  { "date":"11/12/16", "home":"UW", "opponent":"USC", "home_score":13, "opponent_score":26, 
+  { "date":"11/12/16", "home":"UW", "opponent":"USC", "home_score":13, "opponent_score":26,
     "passing_yards":259, "rushing_yards":17, "fumbles":0 },
-  { "date":"11/19/16", "home":"UW", "opponent":"Arizona State", "home_score":44, 
+  { "date":"11/19/16", "home":"UW", "opponent":"Arizona State", "home_score":44,
     "opponent_score":18, "passing_yards":338, "rushing_yards":201, "fumbles":0 },
-  { "date":"11/25/16", "home":"Washington State", "opponent":"UW", "home_score":17, 
+  { "date":"11/25/16", "home":"Washington State", "opponent":"UW", "home_score":17,
     "opponent_score":45, "passing_yards":342, "rushing_yards":168, "fumbles":1 },
-  { "date":"12/2/16", "home":"Colorado", "opponent":"UW", "home_score":10, 
+  { "date":"12/2/16", "home":"Colorado", "opponent":"UW", "home_score":10,
     "opponent_score":41, "passing_yards":118, "rushing_yards":25, "fumbles":0 },
   { "date":"12/31/16", "home":"UW", "opponent":"Alabama", "home_score":7, 
     "opponent_score":24, "passing_yards":150, "rushing_yards":44, "fumbles":1 }
@@ -37,43 +37,61 @@ let huskyGames2016 = [
 //Define a function `extractOpponent()` that takes in a "game" object and returns
 //UW's opponent (whether or not that was the home team!)
 //You can test this by passing in an individual element from the array.
-
+const extractOpponent = (game) => {
+  return game.opponent === "UW" ? game.home : game.opponent;
+};
 
 //Use the `map()` method and your `extractOpponent()` function to create an array
 //of UW's opponents for the season (in the same order as in the `huskyGames2016`).
 //The opponents in the list do not need to be unique.
 //Log out the opponents array.
-
+let opponents =huskyGames2016.map(x => {
+  return extractOpponent(x);
+});
+console.log(opponents);
 
 //Define a function `huskiesLost()` that takes in a "game" object and returns
 //whether or not UW lost.
-
+const huskiesLost = (game) => {
+  return game.opponent !== "UW" ? game.home_score < game.opponent_score : game.home_score > game.opponent_score;
+};
 
 //Use the `filter()` method to create an array of games that UW lost (a smaller
 //array than the games they won!)
 //Log out the array of lost games.
-
+let lostGame = huskyGames2016.filter(huskiesLost);
+console.log(lostGame);
 
 //Log out an array of opponents that UW lost to. Hint: Use the `.map()` method 
 //to extract the opponent names!
-
+let lostOpponents = lostGame.map(x => {
+  return extractOpponent(x);
+});
+console.log(lostOpponents);
 
 //Use a `forEach()` loop to log out each of the games UW lost, each on its own 
 //line, in the following format:
 //    "Rutgers at UW, 13 to 48"
 //You should use an anonymous callback function.
-
+lostGame.forEach(x => {
+  let [opponentScore, uwScore] =
+    x.opponent === "UW" ? [x.home_score, x.opponent_score] : [x.opponent_score, x.home_score];
+  console.log(extractOpponent(x) + " at UW, " + opponentScore + " to " + uwScore);
+});
 
 //Use the `filter()` method with an anonymous callback function to get an array
 //of games where UW had at least one fumble.
 //Log out HOW MANY games included fumbles.
-
+console.log(huskyGames2016.filter(x => x.fumbles > 0).length);
 
 //Define a function `mostYardsPassing()` that takes in two "game" objects and
 //returns the game that has a greater number of passing yards.
 //Your function should handle the case where the _first_ game has no 
 //`passing_yards` property, in which case it should return the second game.
-
+const mostYardsPassing = (previous, current) => {
+  return !previous.passing_yards ? current :
+    (previous.passing_yards >= current.passing_yards ? previous : current);
+};
 
 //Create a variable `mostPassingGame` that refers to the "game" that had the most
 //passing yards in the season. Use the `reduce()` method with `mostYardsPassing()`
@@ -83,8 +101,8 @@ let huskyGames2016 = [
 // - Consider: why do this with `reduce()` instead of `filter()`?
 //
 //Log out the game with the most passing yards.
-
-
+let mostPassingGame = huskyGames2016.reduce(mostYardsPassing, {});
+console.log(mostPassingGame);
 
 //It would be useful to be able to apply multiple "filter criteria" to an array
 //of games at once.
@@ -94,6 +112,11 @@ let huskyGames2016 = [
 //game object and returns the result of passing that object to both of the 
 //callback functions and "anding" (&&) the results. The `makeCombinedFilter()` 
 //function should then return this new function.
+const makeCombinedFilter = (a, b) => {
+  return (x) => {
+    return a(x) && b(x);
+  };
+};
 
 
 //Create a variable `fumbledAndLostFilter` which is the result of calling the 
@@ -102,12 +125,13 @@ let huskyGames2016 = [
 //one for filtering for games with fumbles (this can be a named or an anonymous
 //callback like you used earlier).
 //Note that `fumbledAndLostFilter` _is_ a function!
-
+const fumbledAndLostFilter = makeCombinedFilter(huskiesLost, x => x.fumbles > 0);
 
 //Create an array of games that UW lost with fumbles. Use the 
 //`fumbledAndLostFilter()` function as a callback to the `filter()` method.
 //Log out the array of games lost with fumbles.
-
+let fumbledAndLostGames = huskyGames2016.filter(x => fumbledAndLostFilter(x));
+console.log(fumbledAndLostGames);
 
 
 //OPTIONAL extra practice: create a variable `avgScoreDifference` that
